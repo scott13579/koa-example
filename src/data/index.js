a8b1b8bd2d6bff45573ae51d0bfce97fb3e93751
scott13579 <scott135@naver.com>
2023-01-17 12:32:50 +0900

    mysql 클라이언트 접속 대기 코드 완성

---
+++ src/data/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+/**
+ * Connection Pool
+ */
+exports.connection = mysql.createPool(
+    {
+        host : process.env.DB_HOST,
+        user : process.env.DB_USERNAME,
+        password : process.enc.DB_PASSWORD,
+        database : process.env.DB_DATABASE,
+        waitForConnections : true,
+        connectionLimit : 10,
+        queueLimit : 0
+    }
+);
+
+/**
+ * 조금 더 간단하게 connection pool을 사용할 수 있도록 만든 함수
+ * @param {string} queryString 쿼리 문자열 
+ * @param {array} params 쿼리 ? 에 들어갈 파라미터들
+ * @returns 
+ */
+exports.pool = (queryString, params) =>{
+    return new Promise((resolve, reject) =>{
+        this.connection.query(queryString, params, (err, row, fields) => {
+            (err) ? reject(err) : resolve(rows);
+        });
+    });
+}
